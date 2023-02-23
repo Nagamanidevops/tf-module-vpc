0a18877cab230f482b11ab17e9c36285e461470b
nagamani <nagamanichavaaws@gmail.com>
2023-02-23 08:00:09 +0000

    id

--- main.tf
+++ main.tf
@@ -35,7 +35,11 @@ resource "aws_route" "default" {
   route_table_id            = aws_vpc.main.default_route_table_id
  # route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = "aws_vpc_peering_connection.peer.id"
+ # vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+ # pcx-00df54d61d43b8a48
+
+
 }
 
 
