f33fb7c435c003a08edaa21e1fbd4d8460abfd74
nagamani <nagamanichavaaws@gmail.com>
2023-02-23 07:53:02 +0000

    route

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ resource "aws_subnet" "main" {
   
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
- # peer_vpc_id   = var.default_vpc_id
-  peer_vpc_id   = "vpc-00682b9d34c0f8605"
+  peer_vpc_id   = var.default_vpc_id
+ # peer_vpc_id   = "vpc-00682b9d34c0f8605"
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
   tags = merge(
@@ -29,6 +29,13 @@ resource "aws_vpc_peering_connection" "peer" {
     { Name = "${var.env}-peering" }
   )
 
+}
+
+resource "aws_route" "default" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+ # route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = "aws_vpc_peering_connection.peer.id"
 }
 
 
