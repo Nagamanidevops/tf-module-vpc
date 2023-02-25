f2d6420bcb28dcb27ded46cc14b4ac31cac9770e
nagamani <nagamanichavaaws@gmail.com>
2023-02-25 06:51:02 +0000

    id

--- main.tf
+++ main.tf
@@ -69,7 +69,8 @@ resource "aws_route_table" "public" {
 
 route {
     cidr_block = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = vpc_peering_connection_id.peer.id
+    #vpc_peering_connection_id = vpc_peering_connection_id.peer.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
  
   tags = merge(
