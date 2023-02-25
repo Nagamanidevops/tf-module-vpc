e0c9b7c44f4aa15f2a745589e5baf7b945c249cd
nagamani <nagamanichavaaws@gmail.com>
2023-02-25 07:07:12 +0000

    index

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ route {
 
 resource "aws_route_table_association" "pub-routetble-assoc" {
   count = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public.*.id(count.index)
+  subnet_id      = aws_subnet.public.*.id[count.index]
   route_table_id = aws_route_table.public.id
 }
 
