44be4639fa782857f564ebe9a6e861ad3e49e9b6
nagamani <nagamanichavaaws@gmail.com>
2023-03-04 06:00:10 +0000

    route

--- main.tf
+++ main.tf
@@ -19,4 +19,9 @@ resource "aws_vpc_peering_connection" "peer" {
 
 }
 
+resource "aws_route" "r" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 
