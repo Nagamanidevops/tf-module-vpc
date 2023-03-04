3e920bb1061422c957e94f0dd3a7c7a096b558bc
nagamani <nagamanichavaaws@gmail.com>
2023-03-04 08:04:18 +0000

    comment

--- subnets.tf
+++ subnets.tf
@@ -1,26 +1,26 @@
-module "subnets"{
-  source                        = "./subnets"
+# module "subnets"{
+#   source                        = "./subnets"
   
-    availability_zone           = var.availability_zone
-    default_vpc_id              = var.default_vpc_id
-    env                         = var.env
+#     availability_zone           = var.availability_zone
+#     default_vpc_id              = var.default_vpc_id
+#     env                         = var.env
     
-    for_each                    = var.subnets
-    cidr_block                  = each.value.cidr_block
-    name                        = each.value.name
-    # internet_gw                 =  each.value.internet_gw ? aws_internet_gateway.gw.id : null
-    internet_gw                 =  lookup(each.value, "internet_gw", false)
+#     for_each                    = var.subnets
+#     cidr_block                  = each.value.cidr_block
+#     name                        = each.value.name
+#     # internet_gw                 =  each.value.internet_gw ? aws_internet_gateway.gw.id : null
+#     internet_gw                 =  lookup(each.value, "internet_gw", false)
    
-    # nat_gw = lookup(each.value, "nat_gw", false)
+#     # nat_gw = lookup(each.value, "nat_gw", false)
 
 
     
-     vpc_id                     =  aws_vpc.main.id
-     vpc_peering_connection_id  =  aws_vpc_peering_connection.peer.id
-     tags                       = local.common_tags
-     gateway_id                 = aws_internet_gateway.gw.id
+#      vpc_id                     =  aws_vpc.main.id
+#      vpc_peering_connection_id  =  aws_vpc_peering_connection.peer.id
+#      tags                       = local.common_tags
+#      gateway_id                 = aws_internet_gateway.gw.id
     
-}
+# }
      
      
      
