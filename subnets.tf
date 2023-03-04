2479435f6ea81a310e7d1feb722083115e55f5e0
nagamani <nagamanichavaaws@gmail.com>
2023-03-04 11:36:48 +0000

    a

--- subnets.tf
+++ subnets.tf
@@ -21,23 +21,25 @@ module "public_subnets" {
 }
 
 
-# module "private_subnets" {
+module "private_subnets" {
     
-#      source                        = "./subnets"
+     source                        = "./subnets"
   
-#     availability_zone           = var.availability_zone
-#     default_vpc_id              = var.default_vpc_id
-#     env                         = var.env
+    availability_zone           = var.availability_zone
+    default_vpc_id              = var.default_vpc_id
+    env                         = var.env
     
-#     for_each                    = var.public_subnets
-#     cidr_block                  = each.value.cidr_block
-#     name                        = each.value.name
-#     internet_gw                 =  lookup(each.value, "internet_gw", false)
+    for_each                    = var.private_subnets
+    cidr_block                  = each.value.cidr_block
+    name                        = each.value.name
+    internet_gw                 =  lookup(each.value, "internet_gw", false)
+    nat_gw                     =  lookup(each.value, "nat_gw", false)
    
 
-#      vpc_id                     =  aws_vpc.main.id
-#      vpc_peering_connection_id  =  aws_vpc_peering_connection.peer.id
-#      tags                       = local.common_tags
-#      gateway_id                 = aws_internet_gateway.gw.id
+     vpc_id                     =  aws_vpc.main.id
+     vpc_peering_connection_id  =  aws_vpc_peering_connection.peer.id
+     tags                       = local.common_tags
+     gateway_id                 = aws_internet_gateway.gw.id
+     nat_gw_id                  = aws_nat_gateway.id
     
-# }
+}
