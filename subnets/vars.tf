2d66ff960c25f92a239e2278d132c34de39cfb72
nagamani <nagamanichavaaws@gmail.com>
2023-03-04 12:15:47 +0000

    sa

--- subnets/vars.tf
+++ subnets/vars.tf
@@ -9,5 +9,9 @@ variable "vpc_peering_connection_id" {}
 variable "tags" {}
 variable "internet_gw" {}
 variable "nat_gw" {}
-variable  "gateway_id" {}
-variable "nat_gw_id" {}
+variable  "gateway_id" {
+    default = null
+}
+variable "nat_gw_id" {
+    default = null
+}
