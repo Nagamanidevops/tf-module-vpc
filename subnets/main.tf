e8395256649db23d303c93ca41617f335ef144be
nagamani <nagamanichavaaws@gmail.com>
2023-03-04 07:53:58 +0000

    f

--- subnets/main.tf
+++ subnets/main.tf
@@ -36,7 +36,7 @@ count = length(aws_subnet.main)
 }
 
 resource "aws_route" "internet_gw_route" {
-  count                     = var.internet_gw == null ? 1 : 0
+  count                     = var.internet_gw ? 1 : 0
   route_table_id            = aws_route_table.route_table.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = var.gateway_id
