67fba1a0eca3e474ee8b8eb9439a38377da9d62b
nagamani <nagamanichavaaws@gmail.com>
2023-03-06 11:23:20 +0000

    pvt

--- outputs.tf
+++ outputs.tf
@@ -10,9 +10,8 @@ output "public_subnet_ids" {
     value = module.public_subnets
 }
 
-# output "one_subnet_id"{
-# //value = module.public_subnets["public"]["subnet_ids"][0]
+output "private_subnet_ids" {
+    value = module.private_subnets
+}
 
-#  //value = lookup(lookup(module.public_subnets, "public", null ),"subnet_ids",null)[0]
-# value = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
 
